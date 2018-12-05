6f4dc67c48d9a91aa8e7f73e032155c3e88c347c
Yan Sokolikov <sokolikovyan@yandex.ru>
2018-12-05 23:58:41 +0300

    last commit

--- Curl.php
+++ Curl.php
@@ -2,13 +2,21 @@
 
 class Curl
 {
-    public static function GetString($params, $url = 'https://syn.su/testwork.php'){
+    /**
+     * @param $params
+     * @param string $url
+     * @return mixed
+     *
+     * Здесь инициализируем, далее куда,
+     * даем метод пост, передаем параметры
+     */
+
+    public static function GetJson($params, $url = 'https://syn.su/testwork.php'){
         $curl = curl_init();
 
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_POST, 1);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
-
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
         $json_out = json_decode(curl_exec($curl), true);
@@ -18,18 +26,38 @@ class Curl
         return $json_out;
     }
 
+    /**
+     * @return array
+     * функция опрделения прошло ли более часа с момента последнего выполнения скрипта
+     */
     public static function GetCronTime(){
 
         $last_cron_time = file_get_contents('cron.txt');
-
         $this_time = time();
 
-        if (($last_cron_time + 0) <= $this_time) {
-
+        if (($last_cron_time + 3600) <= $this_time) {
             file_put_contents('cron.txt',time());
-
             return array('1'=>$last_cron_time, '2'=>$this_time);
         }
+    }
+
+    /**
+     * @param $string
+     * @param $key
+     * @return string
+     *
+     * Функция преобразовния строки
+     */
+    public static function StrToXor($string, $key){
+
+        $outText = '';
+
+        for($i = 0; $i < strlen($string); ) {
+            for($j = 0; ($j < strlen($key) && $i < strlen($string)); $j++,$i++) {
+                $outText .= $string{$i} ^ $key{$j};
+            }
+        }
+        return $outText;
     }
 
 }
--- post_otput.php
+++ post_otput.php
@@ -1,19 +1,55 @@
 <?php
+/**
+ * Какая логика у всего этого...
+ *
+ * Есть файл с классами:
+ * 1)для возврата массива данных из post запросов GetJson
+ *
+ * 2)для определения времени, а именно прошло ли меньше часа с момента последнего выполнения скрипта
+ *
+ * 3)для шифрования строки XOR методом
+ *
+ * Скрипт Cron-ом выполняется 1 раз в 10 минут.
+ * При этом в файл cron.txt записывается время
+ * последнего выполения скрипта, далее вычесляется
+ * разница и ,если , прошло вемени больше часа или
+ * равное часу с момента последнего выплнения, то он
+ * снова делает запрос с параметрами (method=UPDATE&message=)
+ *
+ * Чтобы настроить выполнение крона на каждые 10 минут,
+ * нужно просто настроить его на сервере, установив пакет php-5-cli и
+ * почтовый клиент apt-get install mutt и набрав в crontab -e
+ *
+ *  #/10 # # # # /usr/bin/php -q path/to/phpscript/post_output.php > path/to/logtxt/cron.txt && mutt example@email.com -s cron-result < path/to/logtxt/cron.txt
+ *
+ * , тогда в случае ошибки логи будут приходить на почту.
+ *
+ */
+
 
 require __DIR__ . '/Curl.php';
 
-$responseJson = Curl::GetString('method=get');
+$responseJson = Curl::GetJson('method=get');
+try{
+    if (isset($responseJson['response']['message'])){
+        if(!is_null(Curl::GetCronTime())){
 
-if (isset($responseJson['response']['message'])){
-    if(Curl::GetCronTime() != ""){
-        echo base64_encode($responseJson['response']['message']);
-        $params = 'method=UPDATE&message=' . base64_encode($responseJson['response']['message']);
+            $string = $responseJson['response']['message'];
+            $key = $responseJson['response']['key'];
 
-        echo "<pre>";
-        print_r(Curl::GetString($params));
-        echo "</pre>";
+            $params = 'method=update&message=' . base64_encode(Curl::StrToXor($string, $key));
+
+            echo "<pre>";
+            print_r(Curl::GetJson($params));
+            echo "</pre>";
+
+        } else {
+            echo "Подождите...";
+        }
+    } else {
+        throw new Exception("Не удалось получить ответ от сервера...");
     }
-} else {
-    echo 'Увы, что-то пошло не так...';
+} catch (Exception $e){
+    echo $e->getMessage();
 }
 
